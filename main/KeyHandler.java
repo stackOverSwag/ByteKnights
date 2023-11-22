7f6490b2acbe93828501c30ba7e9ba8d44f5a5b6
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2023-11-22 17:33:51 +0100

    Update KeyHandler.java

--- main/KeyHandler.java
+++ main/KeyHandler.java
@@ -1,1 +1,29 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
 
+public class KeyHandler implements KeyListener {
+
+    public boolean escape;
+
+    @Override
+    public void keyTyped(KeyEvent e) {} // NOT USED EVER
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode(); // returns number of key pressed
+        
+        if (code == KeyEvent.VK_ESCAPE) {
+            escape = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {} // NOT USED EVER
+
+    public boolean Pause(){
+        boolean pause = this.escape;
+        this.escape = false;
+        return pause;
+    }
+}
