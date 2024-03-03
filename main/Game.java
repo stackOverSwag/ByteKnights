5d5b85fb9c7cedb478e0486daf5df4c9c813150a
Al Bel <albellebvas@gmail.com>
2024-03-03 23:25:09 +0100

    Signed-off-by: Al Bel <albellebvas@gmail.com>

---
+++ main/Game.java
@@ -0,0 +1,19 @@
+package projet;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+public class Game{
+  ArrayList<Hosts> hosts;
+
+  public Game(){
+
+
+  }
+
+
+
+
+
+}
---
+++ main/Registre.java
@@ -0,0 +1,13 @@
+
+package projet;
+
+// hmmmmmmmmm je sais pas, et toi? tu va bien?
+public class Registre{
+	private String nom;
+
+	public String getNom(){
+		return this.nom;
+	}
+
+
+}
