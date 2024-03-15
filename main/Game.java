bbaedff9fb1edbffd77350da874644410e3d1175
Al Bel <albellebvas@gmail.com>
2024-03-15 20:30:57 +0100

    Signed-off-by: Al Bel <albellebvas@gmail.com>

--- main/Game.java
+++ main/Game.java
@@ -4,10 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Game {
-  ArrayList<Hosts> hosts;
+  private Niveau level;
+  private ArrayList<Parser> parsers;
 
-  public Game() {
+  public Game(Niveau level) {
+    this.level = level;
+    this.parsers = new ArrayList<Parser>();
+    this.parsers.add(new Parser(this.level, this.level.getExa1()));
+    this.parsers.add(new Parser(this.level, this.level.getExa2()));
+  }
 
+  public void step() {
+    for (Parser p : this.parsers) {
+      try {
+        p.step();
+
+      } catch (IllegalArgumentException e) {
+        System.out.println("line " + p.getCurLine() + ": " + e.getMessage());
+      }
+    }
   }
 
 }
--- main/Main.java
+++ main/Main.java
@@ -1,17 +1,31 @@
 package main;
 
+import java.util.ArrayList;
+
 import javax.swing.*;
 
-public class main {
+public class Main {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeater(new Runnable() {
-            @Override
-            public void run() {
-                MainWindow main = new MainWindow();
-                main.show();
-            }
-        });
+    public static void Main(String[] args) {
+        /*
+         * SwingUtilities.invokeater(new Runnable() {
+         * 
+         * @Override
+         * public void run() {
+         * MainWindow main = new MainWindow();
+         * main.show();
+         * }
+         * });
+         */
+
+        Exas a = new Exas("a");
+        a.setCode("ADDI 2 1 X");
+        Exas b = new Exas("b");
+        b.setCode("SUBI 2 1 X");
+        ArrayList<Files> f = new ArrayList<Files>();
+        Niveau level = new Niveau(a, b, f);
+
+        Game game = new Game(level);
     }
 
 }
